2b557edff4662506d9ad0c9e99e93babe541b7c5
Ananya Nair <an.nair1702@gmail.com>
2023-10-20 20:20:04 +0530

    Created sending message API

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,36 @@ import express from 'express';
 //create app using constructor of express
 const app = express();
 app.use(express.json())
+// app.use(cors())
+
+let chats = []
+
+// API
+function send(req, res) {
+    let msg = req.body.msg;
+    let sender = req.body.sender;
+    let receiver = req.body.receiver;
+    chats.push({
+        "msg": msg, 
+        "sender": sender,
+        "receiver": receiver,
+        "time": new Date(),
+        "isSeen": false
+    })
+    res.send("Sent")
+}
+
+function getAllMessages(req, res) {
+    let receiver = req.body.receiver;
+    let relevantChats = chats.filter(chat => chat.receiver === receiver)
+    // TC: O(N)
+    res.send(relevantChats);
+}
+
+// GET, POST
+app.post('/send', send)
+
+app.get('/getAllMessages', getAllMessages)
+
+// port
+app.listen(3007)
